20b45c693b525c4fc62a3890430e447c33060819
xiaozhang <xiaozhang@deepglint.com>
2019-02-15 09:24:13 +0000

    up deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 cd ~
 rm -f .vimrc .tmux.conf .bash_aliases .gitconfig .inputrc  
-ln -s work/huismiling/work_space/_vimrc .vimrc  
-ln -s work/huismiling/work_space/_tmux.conf .tmux.conf  
-ln -s work/huismiling/work_space/_bash_aliases .bash_aliases  
-ln -s work/huismiling/work_space/_gitconfig .gitconfig  
-ln -s work/huismiling/work_space/_inputrc  .inputrc  
+cp work_space/_vimrc .vimrc  
+cp work_space/_tmux.conf .tmux.conf  
+cp work_space/_bash_aliases .bash_aliases  
+cp work_space/_gitconfig .gitconfig  
+cp work_space/_inputrc  .inputrc  
 
 cd -
 
