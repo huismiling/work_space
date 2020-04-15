ca436a15d3f3da6fea616d4cbf2600ef7e646cc2
xiaozhang <xiaozhang@deepglint.com>
2020-04-15 08:52:31 +0800

    up

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
+work_space=$PWD
 cd ~
 rm -f .vimrc .tmux.conf .bash_aliases .gitconfig .inputrc  
-cp work_space/_vimrc .vimrc  
-cp work_space/_tmux.conf .tmux.conf  
-cp work_space/_bash_aliases .bash_aliases  
-cp work_space/_gitconfig .gitconfig  
-cp work_space/_inputrc  .inputrc  
-cp work_space/_pip .pip -r
+cp $work_space/_vimrc .vimrc  
+cp $work_space/_tmux.conf .tmux.conf  
+cp $work_space/_bash_aliases .bash_aliases  
+cp $work_space/_gitconfig .gitconfig  
+cp $work_space/_inputrc  .inputrc  
+cp $work_space/_pip .pip -r
 
 cd -
 
